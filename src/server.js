7d5d922b3c12715e42c2d7a244518eaba8198a0a
leaneroov <leaneroov@gmail.com>
2025-02-26 23:16:23 +0900

    refactor : update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import "./db.js"
 import "./models/Video.js";
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter.js";
+import rootRouter from "./routers/rootRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import userRouter from "./routers/userRouter.js";
 
@@ -15,7 +15,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
