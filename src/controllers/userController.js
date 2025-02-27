68b66c436c6706b86ab2f701c550d6dca5f02f41
leaneroov <leaneroov@gmail.com>
2025-02-27 13:44:51 +0900

    feat : Encrypted PW verification

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,17 +18,46 @@ export const postJoin = async (req, res) => {
     });
   }
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit user");
 export const remove = (req, res) => res.send("Remove user");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See user");
